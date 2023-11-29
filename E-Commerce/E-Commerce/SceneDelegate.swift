f2744e0b4aa89e3d30ceecb3024201fedb7c5379
Ailin Patino <apatino2@lululemon.com>
2023-11-29 15:21:15 -0300

    fix splash screen

--- E-Commerce/E-Commerce/SceneDelegate.swift
+++ E-Commerce/E-Commerce/SceneDelegate.swift
@@ -16,23 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        // Create a new UIWindow using the windowScene constructor
-        let window = UIWindow(windowScene: windowScene)
-        
-        // Create and set the root view controller to your SplashViewController
-        let splashViewController = SplashScreenViewController()
-        window.rootViewController = splashViewController
-        
-        // Set the window's background color (optional)
-        window.backgroundColor = UIColor.white
-        
-        // Set the window to be visible
-        window.makeKeyAndVisible()
-        
-        // Set the UIWindow instance to the window property of the SceneDelegate
-        self.window = window
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
