d0669c0e4e7172e37fbb3e618197f1d5163dad76
Nicolas <nico.marchioli@gmail.com>
2023-12-26 16:56:31 -0300

    table view debuggin

--- E-Commerce/E-Commerce/Presentation/Views/Home/HomeViewController.swift
+++ E-Commerce/E-Commerce/Presentation/Views/Home/HomeViewController.swift
@@ -11,30 +11,41 @@ class HomeViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
-    var strings: [String] = ["Alpha","Beta","Unlimited","Revised",]
+    var names: [String] = ["Alpha","Beta","Unlimited","Revised"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //tableView.register(MyCellTableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
         
-        tableView.dataSource? = self
-        tableView.reloadData()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.reloadData()
+    }
 }
 
-extension HomeViewController: UITableViewDataSource{
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return strings.count
+        return names.count
     }
     
     //What kind of cell you want to return
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MyCellTableViewCell{
+            let value = names[indexPath.row]
+            cell.titleLabel?.text = names[indexPath.row]
+            cell.titleLabel?.textColor = .black
+            print(cell.titleLabel?.text)
+            
+            return cell
+        } else {
+            return UITableViewCell()
+        }
         
-        cell.textLabel?.text = strings[indexPath.row]
-        
-        return cell
     }
     
     
