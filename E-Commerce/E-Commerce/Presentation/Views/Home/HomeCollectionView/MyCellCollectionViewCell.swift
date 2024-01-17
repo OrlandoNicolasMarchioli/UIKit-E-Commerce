d2dddae954a0910c7de80d74f96bd6a80b49a80c
Nicolas <nico.marchioli@gmail.com>
2024-01-17 10:28:49 -0300

    Adding functionality to collection view

--- E-Commerce/E-Commerce/Presentation/Views/Home/HomeCollectionView/MyCellCollectionViewCell.swift
+++ E-Commerce/E-Commerce/Presentation/Views/Home/HomeCollectionView/MyCellCollectionViewCell.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 class MyCellCollectionViewCell: UICollectionViewCell {
-
-    @IBOutlet var itemName: UILabel!
- 
-    @IBOutlet var itemImage: UIView!
-    @IBOutlet var itemPrice: UIView!
     
+    
+    @IBOutlet var itemImage: UIImageView!
+    @IBOutlet var itemPrice: UILabel!
+    @IBOutlet var itemName: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configure(product: Product){
+        self.itemName.text = "$ " + product.name
+        self.itemPrice.text = String(product.price)
     }
 
 }
