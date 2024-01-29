a806820684a7fe9cdc3b5b6fda80a56d9096b1e0
Nicolas <nico.marchioli@gmail.com>
2024-01-29 16:19:11 -0300

    Adding cart controller and correcting some collection issues

--- E-Commerce/E-Commerce/Presentation/Views/Home/HomeTableView/HomeTableViewCell.swift
+++ E-Commerce/E-Commerce/Presentation/Views/Home/HomeTableView/HomeTableViewCell.swift
@@ -10,24 +10,48 @@ import UIKit
 class HomeTableViewCell: UITableViewCell {
 
     @IBOutlet var itemName: UILabel!
+
+    @IBOutlet var itemEliminate: UIButton!
+    @IBOutlet var itemImage: UIImageView!
     @IBOutlet var itemType: UILabel!
     @IBOutlet var itemPrice: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        itemEliminate.setImage(UIImage(systemName: "trash"), for: .normal)
+        itemEliminate.setTitle("", for: .normal)
+        itemEliminate.imageView?.contentMode = .scaleAspectFit
+        itemEliminate.layer.cornerRadius = 10
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        contentView.layer.cornerRadius = 10
+        contentView.layer.masksToBounds = true
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.lightGray.cgColor
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.2
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 5
+        layer.masksToBounds = false
+        
+        
     }
     
     func configure(product: Product){
         self.itemName.text = "$ " + product.name
         self.itemType.text = product.type
         self.itemPrice.text = String(product.price)
+        self.itemImage.image = imageForName(name: product.image)
+    }
+    
+    func imageForName(name: String) -> UIImage?{
+        return UIImage(named: name)
     }
     
 }
--- E-Commerce/E-Commerce/Presentation/Views/Home/HomeViewController.swift
+++ E-Commerce/E-Commerce/Presentation/Views/Home/HomeViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-
+    
+    @IBOutlet var starButton: UIButton!
+  
+    @IBOutlet var cartButton: UIButton!
     @IBOutlet var horizontalTableView: UICollectionView!
     @IBOutlet var verticalTableView: UITableView!
-    
+    let verticalCellSpacing: CGFloat = 70.0
     let collectionViewLayout = MyCellCollectionViewCellLayout()
     
     var products: [Product] = [Product(image: "vino", name: "Vino toro", type: "bebidas", price: 2500.0),Product(image: "aceituna", name: "Aceitunas sin carozo", type: "frutas", price: 1000.0),Product(image: "cervezaCorona", name: "Cerveza corona 300cc", type: "bebidas", price: 2500),Product(image: "quesoFresco", name: "Queso fresco ", type: "quesos", price: 5460.0),Product(image: "LecheEnteraLaSerenisima", name: "Leche entera 1L", type: "bebidas s/ alcohol", price: 1500),Product(image: "anana", name: "Anana", type: "frutas", price: 750)]
@@ -20,7 +23,8 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        let navigation = self.navigationController
+//        self.navigationController?.navigationBar
         let nib = UINib(nibName: "HomeTableViewCell", bundle: nil)
         let collectionNib = UINib(nibName: "MyCellCollectionViewCell", bundle: nil)
         horizontalTableView.collectionViewLayout = collectionViewLayout
@@ -32,12 +36,38 @@ class HomeViewController: UIViewController {
         verticalTableView.dataSource = self
         verticalTableView.delegate = self
         
+        
+        cartButton.setImage(UIImage(systemName: "cart"), for: .normal)
+        cartButton.setTitle("", for: .normal)
+        cartButton.imageView?.contentMode = .scaleAspectFit
+        cartButton.addTarget(self, action: #selector(showChart), for: .touchUpInside)
+        cartButton.layer.cornerRadius = 10
+        
+        self.cartButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+        starButton.setImage(UIImage(systemName: "star"), for: .normal)
+        starButton.setTitle("", for: .normal)
+        starButton.imageView?.contentMode = .scaleAspectFit
+        starButton.imageView?.tintColor = UIColor.white
+        starButton.layer.cornerRadius = 10
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         verticalTableView.reloadData()
         horizontalTableView.reloadData()
+    }
+    
+    @objc func showChart() {
+        let chartViewController = ChartViewController()
+        navigationController?.pushViewController(chartViewController, animated: true)
+    }
+    
+    @objc private func didTapButton() {
+        let chartController = ChartViewController()
+        chartController.productsToBuy = products
+        navigationController?.pushViewController(chartController, animated: true)
+        print("Button pressed")
     }
 }
 
@@ -55,6 +85,14 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate{
         cell.configure(product: product)
         return cell
         
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return  verticalCellSpacing
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return verticalCellSpacing
     }
 }
 
